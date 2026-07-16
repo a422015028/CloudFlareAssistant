b6c48824ec26e0591a84adea910494675526f6d7
a422015028 <55303265+a422015028@users.noreply.github.com>
2026-07-16 12:04:33 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.muort.upworker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2026071402
+        versionCode = 2026071601
         versionName = "7.6.3"
         
         vectorDrawables { 
